0a39582839de4869409c678ab917a64d9b70769d
spiricnra2052019 <106819752+spiricnra2052019@users.noreply.github.com>
2023-05-30 18:40:10 +0200

    ratins beckedn finished

---
+++ src/main/java/com/gateway/apigateway/controller/RateAccommodationController.java
@@ -0,0 +1,32 @@
+package com.gateway.apigateway.controller;
+
+import com.gateway.apigateway.model.Rate;
+import com.gateway.apigateway.model.RateAccommodation;
+import com.gateway.apigateway.service.RateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/rate-accommodation")
+@RequiredArgsConstructor
+public class RateAccommodationController {
+
+    @Autowired
+    RateService rateService;
+
+    @PostMapping("")
+    public ResponseEntity rateAccommodation(@RequestBody RateAccommodation rate){
+        return ResponseEntity.status(HttpStatus.CREATED).body(rateService.rateAccommodation(rate));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity changeAccommodationRate(@RequestBody RateAccommodation rate){
+        return ResponseEntity.status(HttpStatus.CREATED).body(rateService.changeAccommodationRate(rate));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteAccommodationRate(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(rateService.deleteAccommodationRate(id));
+    }
+}
--- src/main/java/com/gateway/apigateway/service/RateService.java
+++ src/main/java/com/gateway/apigateway/service/RateService.java
@@ -1,7 +1,10 @@
 package com.gateway.apigateway.service;
 
+import com.gateway.apigateway.mapper.RateAccommodationMapper;
 import com.gateway.apigateway.mapper.RateMapper;
 import com.gateway.apigateway.model.Rate;
+import com.gateway.apigateway.model.RateAccommodation;
+import communication.AccommodationRate;
 import communication.Id;
 import communication.MessageResponse;
 import communication.rateServiceGrpc;
@@ -52,5 +55,42 @@ public class RateService {
 
         communication.Rate response = blockingStub.deleteRate(request);
         return convertRateRequestToEntityWithId(response);
+    }
+
+    public String rateAccommodation(RateAccommodation rate) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9094)
+                .usePlaintext()
+                .build();
+        rateServiceGrpc.rateServiceBlockingStub blockingStub = rateServiceGrpc.newBlockingStub(channel);
+
+        communication.AccommodationRate request = RateAccommodationMapper.convertFromMessageToRate(rate);
+
+        MessageResponse response = blockingStub.rateAccommodation(request);
+        return response.getMessage();
+    }
+
+    public RateAccommodation changeAccommodationRate(RateAccommodation rate) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9094)
+                .usePlaintext()
+                .build();
+        rateServiceGrpc.rateServiceBlockingStub blockingStub = rateServiceGrpc.newBlockingStub(channel);
+
+        communication.AccommodationRate r = RateAccommodationMapper.convertFromMessageToRateWithId(rate);
+        communication.AccommodationRate response = blockingStub.changeAccommodationRate(r);
+        return RateAccommodationMapper.convertRateRequestToEntityWithId(response);
+    }
+
+    public RateAccommodation deleteAccommodationRate(Long id) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9094)
+                .usePlaintext()
+                .build();
+        rateServiceGrpc.rateServiceBlockingStub blockingStub = rateServiceGrpc.newBlockingStub(channel);
+
+        communication.UserIdRequest request = communication.UserIdRequest.newBuilder()
+                .setId(id)
+                .build();
+
+        communication.AccommodationRate response = blockingStub.deleteAccommodationRate(request);
+        return RateAccommodationMapper.convertRateRequestToEntityWithId(response);
     }
 }
